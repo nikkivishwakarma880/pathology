f247da0d077574f14465631d8a0b378bbef5b808
Nikki Vishwakarma <nikkivishwakarma880@gmail.com>
2026-07-22 12:39:54 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import Navbar from './component/Navbar'
 import Home from './pages/Home'
 import Footer from './component/Footer'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-     <Navbar />
-     <Home /> 
-     <Footer />
-     
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+
     </div>
   )
 }
