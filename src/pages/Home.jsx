186158294967c00eed35044d4710b9057873647b
Nikki Vishwakarma <nikkivishwakarma880@gmail.com>
2026-07-22 12:43:39 +0530

    Contact CTA fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Hero from '../component/home/Hero'
 import AboutUs from '../component/home/AboutUs'
-import ContactCTA from '../component/home/ContentCTA'
 import Gallery from '../component/home/Gallry'
 import OurExpert from '../component/home/OurExpert'
 import Testimonials from '../component/home/Testimonials'
 import FAQ from '../component/home/FAQ'
+import ContactCTA from '../component/home/ContactCTA'
 
 const Home = () => {
   return (
